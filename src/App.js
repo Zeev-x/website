3be2310c51118fd736a1e6e6f0fa63d2a6713b5c
Zeev <87901027+Zeev-x@users.noreply.github.com>
2023-06-19 20:09:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='/assets/logo.svg' className="App-logo" alt="logo" />
+        <img src='./assets/logo.svg' className="App-logo" alt="logo" />
         <p>
           Welcome <strong>User</strong>
         </p>
